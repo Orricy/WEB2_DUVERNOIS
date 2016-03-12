af105c431bfc66bad4747fdba3ba4f2db2e71bbd
Oricy <d.duvernois@student.isartdigital.com>
2016-03-12 11:18:29 +0100

    edit users table
    add is_admin (boolean) default to false
    edit the faker

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,6 +16,7 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
+        'is_admin' => $faker->boolean,
         'remember_token' => str_random(10),
     ];
 });
