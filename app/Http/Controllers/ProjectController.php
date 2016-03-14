2d28ffae66031e7330efbd036c5e9898dbd93cd8
Oricy <d.duvernois@student.isartdigital.com>
2016-03-14 23:58:41 +0100

    add middleware to projects
    force the user to connect in order to add his project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,6 +8,9 @@ use App\Http\Requests;
 
 class ProjectController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
